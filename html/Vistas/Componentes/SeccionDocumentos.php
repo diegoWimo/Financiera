0c7619983248849b14583cf9d2f14c5ddd225109
legolasblack <legolas.black.ds@gmail.com>
2023-04-18 20:29:14 -0600

    cambiamos y dejamos solo 3 cards en el catalogo, corregimos los acomodos de la seccion home seccion documentos

--- html/Vistas/Componentes/SeccionDocumentos.php
+++ html/Vistas/Componentes/SeccionDocumentos.php
@@ -7,20 +7,16 @@
       </div>
       <div class="col-md-7 col-12 order-1 order-md-2">
         <div data-aos="fade-up" data-aos-duration="1500">
-          <h1 class="text-center"><strong>!Obtén tu credito en solo 5 minutos¡</strong></h1>
-          <p class="h5 my-4">En cualquiera de nuestras sucursales podras obtener tu autorizacion de credito y estrenar
-            celular
-            en menos de
-            ¡5 minutos!
-            solo necesitaras los siguientes documnetos.</p>
+          <h1 class="text-center"><strong>!Estrena tu celular hoy¡</strong></h1>
+          <p class="h5 my-4">Visita cualquiera de nuestros puntos de venta y renueva tu celular en tan solo unos minutos. Solo
+necesita estos sencillos requisitos:</p>
         </div>
 
         <div class="contenedor-icono" data-aos="zoom-in-left" data-aos-duration="1000">
           <i class="bi bi-person-vcard icono"></i>
           <div class="info-icono">
             <h4 class="title">INE / IFE</h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque</p>
+            <p>Solo debes presentar tu INE ó IFE actualizado, el cual deberá estar vigente y con tu domicilio actual.</p>
           </div>
         </div>
 
@@ -29,8 +25,7 @@
           <i class="bi bi-card-text icono"></i>
           <div class="info-icono">
             <h4 class="title">CURP</h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque</p>
+            <p>Proporciónanos tu CURP coincidentes con tu INE o IFE.</p>
           </div>
         </div>
 
@@ -38,8 +33,8 @@
           <i class="bi bi-telephone icono"></i>
           <div class="info-icono">
             <h4 class="title">Numero Telefonico</h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque</p>
+            <p>Número de teléfono móvil personal, el cual validaremos por medio de un mensaje de texto al momento que obtengas 
+             tu financiamiento.</p>
           </div>
         </div>
 
@@ -48,24 +43,21 @@
           <i class="bi bi-people-fill icono"></i>
           <div class="info-icono">
             <h4 class="title">2 Referencias</h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque</p>
+            <p>Con tan solo dos referencias laborales y/o personales puede acceder a tu financiamiento.</p>
           </div>
         </div>
         <div class="contenedor-icono" data-aos="zoom-in-left" data-aos-duration="1000">
         <i class="bi bi-envelope-at icono"></i>
           <div class="info-icono">
             <h4 class="title">Correo Electronico</h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque</p>
+            <p>Proporciónanos un correo electrónico donde podamos contactarte.</p>
           </div>
         </div>
         <div class="contenedor-icono" data-aos="zoom-in-left" data-aos-duration="1000">
         <i class="bi bi-file-earmark-text icono"></i>
           <div class="info-icono">
             <h4 class="title">Comprobante de Domicilio Actual</h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-              voluptatum deleniti atque</p>
+            <p></p>
           </div>
         </div>
 
